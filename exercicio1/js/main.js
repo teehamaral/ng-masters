2cb691bb4c03d2d0e2cfd701a64f680d18f8213f
teehamaral <teeh@agencianic.com.br>
2015-04-27 19:40:21 -0300

    Montagem do HTML do exercício 1

---
+++ exercicio1/js/main.js
@@ -0,0 +1,9 @@
+function buscarasissues(){
+    function GithubRepo(username, reponame){
+    }
+}
+//mais um pouco de codigo aqui....
+var le_repo = GithubRepo(username, reponame);
+    le_repo.busca_issues();
+    le_repo.popula_tabela("#issuestable");
+}
