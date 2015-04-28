134c03811e1435f71b520438226710c277d4ae7c
teehamaral <teeh@agencianic.com.br>
2015-04-27 23:30:59 -0300

    1ª solução do exercício

--- exercicio1/js/main.js
+++ exercicio1/js/main.js
@@ -1,9 +1,38 @@
 function buscarasissues(){
-    function GithubRepo(username, reponame){
-    }
-}
-//mais um pouco de codigo aqui....
-var le_repo = GithubRepo(username, reponame);
-    le_repo.busca_issues();
-    le_repo.popula_tabela("#issuestable");
+	
+	function GithubRepo(username, reponame){
+		var url = 'https://api.github.com/repos/' + username + '/' + reponame + '/issues';
+		var issue = {
+			'buscar_issues': function(){
+				$.get(url).success(function(result){
+					limpa_tabela();
+					for (r in result){
+						popular_tabela(result[r]);	
+					}
+				});
+			},
+		}
+		return issue;
+	}
+
+	function popular_tabela(item){
+		var table = $('#issues');
+		var list = '<tr>';
+		list += '<td>' + item.title + '</td>';
+		list += '<td> <a target="_blank" href="' + item.url + '">' + item.url + ' </a> </td>';
+		list += '<td>' + item.state + '</td>';
+		list += '</tr>';
+		table.append(list);
+	}
+
+	function limpa_tabela(){
+		var list = $('#issues tr');
+		list.remove();
+	}
+
+	var user = $('#user').val();
+	var repositorio = $('#reponame').val();
+	
+	var le_repo = GithubRepo(user, repositorio);
+	le_repo.buscar_issues();
 }
