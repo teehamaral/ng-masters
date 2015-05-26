044fcf859d4829ffd07d3139102307a65b15a454
teehamaral <teeh@agencianic.com.br>
2015-05-26 09:26:59 -0300

    Modificação na frase de exibição do popup

--- exercicio3/js/controller.js
+++ exercicio3/js/controller.js
@@ -3,11 +3,11 @@ angular.module('myApp').controller('MyCtrl', function($scope, MyAlerts){
     $scope.m = MyAlerts;
     
     $scope.mostra_alerta = function(){
-        MyAlerts.showAlert('Você mostrou um alerta');
+        MyAlerts.showAlert('Você mostrou um alerta.');
     };
     
     $scope.mostra_popup = function(){
-        MyAlerts.showPopup('Título do PopUp', 'Uma mensagem muito grande que precisa de um popup pra poder ser visualizada');
+        MyAlerts.showPopup('Você mostrou um popup', 'Uma mensagem muito grande precisa de um popup pra poder ser visualizada.');
     };
     
 });
